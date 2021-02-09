9032128a7932ac7fd2955cb6f2dbdbcf3a6776d5
mdieudonne <michael.dieudonne@outlook.com>
2021-02-09 14:56:02 +0100

    feat: work on GameCard

---
+++ src/components/Card.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="Card">
+    <div class="Suit">
+      <component :is="getSuit" style="height: 30px;" />
+      <component :is="getSuit" style="height: 30px;" />
+    </div>
+    <div class="Face">
+      {{ getFace }}
+    </div>
+    <div class="Suit">
+      <component :is="getSuit" style="height: 30px;" />
+      <component :is="getSuit" style="height: 30px;" />
+    </div>
+  </div>
+</template>
+
+<script>
+import Club from '@/components/icons/IconClub'
+import Spade from '@/components/icons/IconSpade'
+import Heart from '@/components/icons/IconHeart'
+import Diamond from '@/components/icons/IconDiamond'
+
+export default {
+  name: 'Card',
+  props: {
+    card: String,
+  },
+  components: {
+    Club,
+    Spade,
+    Heart,
+    Diamond,
+  },
+  computed: {
+    getFace() {
+      return this.card.split(' ')[0]
+    },
+    getSuit() {
+      return this.card.split(' ')[1]
+    }
+  }
+}
+</script>
+
+<style scoped>
+.Card {
+  border: 2px solid #e7e7e7;
+  border-radius: 4px;
+  padding: 5px;
+  margin: 5px;
+  height: 176px;
+  width: 126px;
+  display: flex;
+  flex-direction: column;
+  background-color: azure;
+}
+
+.Card .Face {
+  font-size: 36px;
+  height: 80%;
+  display: flex;
+  align-content: center;
+  justify-content: center;
+}
+
+.Card .Suit {
+  font-size: 36px;
+  vertical-align: middle;
+  display: flex;
+  justify-content: space-between;
+}
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,144 @@
+<template>
+  <div class="Container">
+    <div v-if="errorMessage" class="Alert">
+      {{ errorMessage }}
+    </div>
+    <div class="Commands">
+      <input v-model="cardsNumber" placeholder="Nombre de cartes">
+      <button v-if="!clearable" @click="onClickSubmit">Recevoir une main</button>
+      <button v-else @click="onClickClear">Rendre les cartes</button>
+      <button @click="onClickSort" v-if="hand.length > 0">Trier la main</button>
+    </div>
+
+    <transition-group name="cardgame" tag="div" class="Cards">
+      <template v-for="card of hand">
+        <card :key="card" :card="card" />
+      </template>
+    </transition-group>
+  </div>
+</template>
+
+<script>
+import Card from '@/components/Card'
+import axios from "axios";
+
+export default {
+  name: "Home",
+  data: () => ({
+    hand: [],
+    cardsNumber: 10,
+    errorMessage: '',
+    clearable: false,
+  }),
+  components: {
+    Card
+  },
+  methods: {
+    clearError() {
+      this.errorMessage = ''
+    },
+    onClickClear() {
+      this.hand = [];
+      this.clearable = false
+    },
+    async onClickSubmit() {
+      try {
+        this.clearError()
+        const response = await axios({
+          method: 'GET',
+          url: 'cards',
+          params: {
+            count: this.cardsNumber,
+          }
+        })
+        this.hand = response.data
+        this.clearable = true
+      } catch (err) {
+        this.onError(err)
+      }
+    },
+    async onClickSort() {
+      try {
+        this.clearError()
+        const response = await axios({
+          method: 'GET',
+          url: 'cards/sort',
+          params: {
+            hand: this.hand,
+          }
+        })
+
+        this.hand = {...response.data}
+      } catch (err) {
+        this.onError(err)
+      }
+    },
+    onError(err) {
+      this.onClickClear()
+      if (err.response) {
+        console.log(err.response)
+        this.errorMessage = '' + err.response.status + ': ' + err.response.data.detail
+      }
+    },
+  },
+}
+</script>
+
+<style scoped lang="css">
+
+.Container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+
+.Alert {
+  padding: 20px;
+  background-color: #f44336; /* Red */
+  color: white;
+  margin-bottom: 15px;
+}
+
+.Commands {
+  padding-bottom: 24px;
+}
+
+.Commands input {
+  width: 200px;
+  padding: 12px 20px;
+  margin: 8px 0;
+  box-sizing: border-box;
+}
+
+.Commands button {
+  padding: 12px;
+  margin-left: 12px;
+  cursor: pointer;
+}
+
+.Cards {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: center;
+
+}
+
+.Card {
+  transition: all 1s;
+}
+
+.cardgame-enter, .cardgame-leave-to
+  /* .list-complete-leave-active below version 2.1.8 */ {
+  opacity: 0;
+  transform: translateY(30px);
+}
+
+.cardgame-active {
+  position: absolute;
+}
+
+.cardgame-move {
+  transition: transform 1s;
+}
+</style>
