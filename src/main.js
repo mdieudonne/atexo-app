1702fc0818b8685137e0282c1684ae9198f6c097
mdieudonne <michael.dieudonne@outlook.com>
2021-02-09 08:47:11 +0100

    chore: install Axios chore: implement .env

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import axios from 'axios'
+
+require('dotenv').config()
+axios.defaults.baseURL = process.env.VUE_APP_API_URL
+axios.defaults.timeout = 10000
 
 Vue.config.productionTip = false
 
